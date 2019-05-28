ec4a16bd04c0a409f3b1706ad3e17eba828d5872
Claus Nielsen <clausn@unity3d.com>
2019-05-28 10:54:28 +0200

    Initial commit, PowerShell config for VSCode Remote Development

---
+++ Docker-Demo.Tests.ps1
@@ -0,0 +1,29 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "New-DockerDemo" {
+
+    $Path = './testfolder'
+    $Name = 'DockerDemo'
+    New-DockerDemo -Path $Path -FileName $Name
+
+    It "Directory Should Exist" {
+        (Test-Path -Path $Path) | Should Be $true
+    }
+    It "Application folder should exist" {
+        (Test-Path "$Path\$Name") | should be $true
+    }
+    It ".config file value should be 'Super Simple Pester Docker test'" {
+        (Get-Content -Path "$Path\$Name.config") | should be 'Super Simple Pester Docker test'
+    }
+}
+
+Describe "Test-Connect" {
+
+    It "Should Exist" {
+        (Test-Connection 127.0.0.1 -Count 1 -Quiet)  | Should Be $true
+    }
+
+
+}
---
+++ Docker-Demo.ps1
@@ -0,0 +1,13 @@
+﻿function New-DockerDemo
+{
+	[CmdletBinding()]
+	param
+	(
+		$Path,
+		$FileName
+	)
+
+	New-Item -Path $Path -Name $FileName -ItemType Directory -Force
+	New-Item -Path "$Path\$FileName" -Name "$FileName.config" -ItemType File -Force
+	Set-Content -Path "$Path\$FileName.config" -Value 'Super Simple Pester Docker test' -Force
+}
